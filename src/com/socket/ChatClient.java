6923455564be4780a1270ad33c1dbaec8ff979ed
KimMinSu <slsajs@naver.com>
2020-08-18 16:20:04 +0900

    소켓 채팅 주석추가

--- src/com/socket/ChatClient.java
+++ src/com/socket/ChatClient.java
@@ -8,29 +8,30 @@ import java.net.Socket;
 
 public class ChatClient {
     public static void main(String[] args) {
-/*        if(args.length != 2){
+        if(args.length != 2){
             System.out.println("사용법 : java chatclient id 접속할 서버 ip");
-            System.exit(1);
-        }*/
+            System.exit(1); //id와 ip를 적으면 배열 개수가 2가 되기 떄문에 만약 2가 아니라면 잘못입력 된 것이므로 프로그램을 종료
+        }
         Socket socket = null;
         BufferedReader br = null;
         PrintWriter pw = null;
         boolean endflag = false;
         try {
-            socket = new Socket("localhost", 10001); // 아이디, 포트
-            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            socket = new Socket(args[1], 10001); //args[1]은 서버 ip로 소켓에 ip주소 전달
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream())); //서버에 데이터를 전달할 수 있는 스트림을 생성한다
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream())); //서버로부터 데이터를 받아 올 수 있는 스트림을 생성한다
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in)); //사용자가 콘솔상에 직접 입력하는 스트림을 생성
 
-        //    pw.println(args[0]);
-            pw.flush();
-            InputThread it = new InputThread(socket, br);
-            it.start();
+            //사용자 id를 전송
+            pw.println(args[0]); //서버로 사용자 id전송
+            pw.flush(); // 버퍼 비우기
+            InputThread it = new InputThread(socket, br); //클라이언트의 스레드, 데이터를 받으면서 동시에 문자를 입력하도록 한다
+            it.start(); //스레드 시작
             String line = null;
-            while ((line = keyboard.readLine()) != null){
-                pw.println(line);
+            while ((line = keyboard.readLine()) != null){ //사용자가 입력한 물자열을 입력받아서 서버로 전송하는 역활
+                pw.println(line); //pw 서버로 이어지는 출력 스트림
                 pw.flush();
-                if(line.equals("/quit")){
+                if(line.equals("/quit")){ //만약 사용자가 /quit를 입력했다면 endflag를 true로 바꾸고 while문 빠져나옴
                     endflag = true;
                     break;
                 }
@@ -56,18 +57,21 @@ public class ChatClient {
     }
 }
 
+// InputThread는 클라이언트 프로그램이 입력을 하는 동시에 데이터를 받을 수 있게 해준다
+// 이 스레드는 들어오는 데이터를 계속해서 받아주는 역활을 한다
+
 class InputThread extends Thread{
     private Socket socket = null;
     private BufferedReader br = null;
 
-    public InputThread(Socket socket, BufferedReader br){
+    public InputThread(Socket socket, BufferedReader br){ //생성과 동시에 소켓 넘버와 서버로부터 데이터를 읽어오는 입력 스트림의 주소를 받는다
         this.socket = socket;
         this.br = br;
     }
     public void run(){
         try {
             String line = null;
-            while ((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null){ // 서버로부터 데이터를 읽음과 동시에 화면에 출력해준다
                 System.out.println(line);
             }
         }catch (Exception e){
--- src/com/socket/ChatServer.java
+++ src/com/socket/ChatServer.java
@@ -13,12 +13,12 @@ import java.util.Iterator;
 public class ChatServer {
     public static void main(String[] args) {
         try {
-            ServerSocket server = new ServerSocket(10001);
-            HashMap<String, Object> hm = new HashMap<String, Object>();
+            ServerSocket server = new ServerSocket(10001); // 서버 소켓 인스턴스 생성 소켓넘버를 파라미터로 받고 클라이언트의 접속을 확인해준다
+            HashMap<String, Object> hm = new HashMap<String, Object>(); // 해시맵 생성
             while (true){
                 System.out.println("접속을 기다립니다");
-                Socket sock = server.accept();
-                ChatThread chatThread = new ChatThread(sock, hm);
+                Socket sock = server.accept(); // 클라이언트의 접속을 확인하고 동시에 소켓인스턴스를 생성
+                ChatThread chatThread = new ChatThread(sock, hm); // 서버 프로그램의 스레드를 생성
                 chatThread.start();
             }
         }catch (Exception e){
@@ -27,6 +27,9 @@ public class ChatServer {
     }
 }
 
+// ChatThread는 서버 프로그램에서 서버의 일을 대신 해준다. 서버는 그저 while문을 통해 접속하는 클라이언트를 받아주는 역활만을 한다
+// 대신 동시에 자신의 일을 대신해줄 수 있는 스레드를 생성하여, 그 스레드에게 서버의 역활을 위임
+
 class ChatThread extends Thread{
     private Socket socket;
     private String id;
@@ -34,15 +37,19 @@ class ChatThread extends Thread{
     private HashMap<String, Object> hm;
     private boolean initFlag = false;
 
+    // 생성자의 파라미터로 소켓 넘버와 해시맵 참조값을 전달
     public ChatThread(Socket socket, HashMap<String,Object> hm){
         this.socket = socket;
         this.hm = hm;
         try {
-            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            id = br.readLine();
-            broadcast(id + "님이 접속하셨습니다.");
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream())); // 클라이언트에게 데이터를 전달 해주는 출력 스트림 생성
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 클라이언트로 부터 데이터를 받을 입력스트림 생성
+            id = br.readLine(); // 맨 첫번째 배열이었던 id를 읽어와 저장
+            broadcast(id + "님이 접속하셨습니다."); // 같이 채팅하는 유저들에게 알림
             System.out.println("접속한 사용자의 아이디 : " + id);
+
+            //해시맵에 id와 서버의 출력스트림 참조값을 저장
+            // 만약 스레드가 동시에 해시맵을 참조한다면, 충돌가능성이 있기 때문에 동기화를 시켜준다
             synchronized (hm){
                 hm.put(this.id, pw);
             }
@@ -57,20 +64,20 @@ class ChatThread extends Thread{
         try {
             String line = null;
             while ((line = br.readLine()) != null){
-                if(line.equals("/quit")){
+                if(line.equals("/quit")){ // 사용자가 종료를 선언하는 문자열을 입력했을때 while문을 빠져나옴
                     break;
                 }
-                if(line.indexOf("/to") == 0){
+                if(line.indexOf("/to") == 0){ // 해당 문자열이 첫번째 글자의 위치 번호를 반환 한다. 만약 "/to"가 첫번째에 있다면 "/"의 위치 번호 0을 반환
                     sendmsg(line);
                 }else {
-                    broadcast(id + " : " + line);
+                    broadcast(id + " : " + line); // 위의 어느 조건도 만족하지 않는 다면 채팅방에 문자를 출력
                 }
             }
         }catch (Exception e){
             e.printStackTrace();
         }finally {
             synchronized (hm){
-                hm.remove(id);
+                hm.remove(id); // 해시맵에 등록된 아이디 삭제
             }
             broadcast(id + "님이 접속을 종료했습니다.");
             try {
@@ -82,26 +89,31 @@ class ChatThread extends Thread{
             }
         }
     }
+
+    // 귓속말 보내는 메소드
     public void sendmsg(String msg){
-        int start = msg.indexOf(" ") + 1;
-        int end = msg.indexOf(" ", start);
+        int start = msg.indexOf(" ") + 1; // " "이 위치하고 있는 번호에 1을 더해 " " 다음이 시작지점임을 저장, 여기서는 "/to" 부분
+        int end = msg.indexOf(" ", start); // start이후 그 다음 " "가 나오는 곳의 위치번호를 저장하여 끝을 알림
         if(end != -1){
-            String to = msg.substring(start,end);
-            String msg2 = msg.substring(end +1);
-            Object obj = hm.get(to);
+            String to = msg.substring(start,end); // 처음번호와 끝번호 사이에 저장되는 문자를 출력
+            String msg2 = msg.substring(end +1); // 끝번호 다음 부터 메세지가 입력 되므로 그다음부터 끝까지의 문자열을 msg2에 저장
+            Object obj = hm.get(to); // to에 해당하는 데이터 즉 출력스트림의 참조값을 오브젝트 인스턴스에 저장, 모든 클래스가 Object 클래스를 상속하기 때문에 가능
             if(obj != null){
-                PrintWriter pw = (PrintWriter)obj;
-                pw.println(id + "님이 다음의 귓속말을 보내셨습니다. : " + msg2);
+                PrintWriter pw = (PrintWriter)obj; // 해당 참조값을 pw에 저장
+                pw.println(id + "님이 다음의 귓속말을 보내셨습니다. : " + msg2); // 해당 출력스트림을 가지고 있는 사람에게 귓속말 전달
                 pw.flush();
             }
         }
     }
     public void broadcast(String msg){
         synchronized (hm){
-            Collection<Object> collection = hm.values();
-            Iterator<?> iter = collection.iterator();
-            while (iter.hasNext()){
-                PrintWriter pw = (PrintWriter)iter.next();
+            Collection<Object> collection = hm.values(); // 해시맵에 저장되어 있는 모든 출력스트림의 참조값을 collection에 저장
+            Iterator<?> iter = collection.iterator(); // iterator를 생성하여 각각의 데이터를 참조하여 접근하도록 한다
+
+            // 결국 해시맵에 저장되어 있는 모든 사람에게 메세지를 전달하는 것이다
+            // 해당 데이터 즉, 출력스트림으로 파라미터로 전달받은 메세지를 전송한다
+            while (iter.hasNext()){ // 데이터가 있다면면
+               PrintWriter pw = (PrintWriter)iter.next();
                 pw.println(msg);
                 pw.flush();
             }
